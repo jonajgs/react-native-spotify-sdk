fda1a55c9ebd057eedb65d622ea3fc834ec973ae
jonathan <jonathangtz.sosa@gmail.com>
2017-06-29 18:50:38 -0500

    set more scopes

--- android/src/main/java/com/reactlibrary/RNSdkSpotifyModule.java
+++ android/src/main/java/com/reactlibrary/RNSdkSpotifyModule.java
@@ -2,10 +2,14 @@ package com.reactlibrary;
 
 import android.util.Log;
 import android.net.Uri;
-import java.util.Map;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.ConnectivityManager;
+import android.app.Activity;
+import android.content.Intent;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.Promise;
@@ -13,6 +17,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -26,82 +34,60 @@ import com.spotify.sdk.android.player.PlaybackState;
 import com.spotify.sdk.android.player.Metadata;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
-
-import android.net.NetworkInfo;
 import com.spotify.sdk.android.player.Connectivity;
-import android.net.ConnectivityManager;
-import android.app.Activity;
-import android.content.Intent;
 
-import com.facebook.react.bridge.ActivityEventListener;
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.WritableMap;
-
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
+
 public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
-    implements ActivityEventListener, Player.NotificationCallback, ConnectionStateCallback{
+    implements ActivityEventListener, Player.NotificationCallback, ConnectionStateCallback {
 
-    private static final String E_NO_ACTIVITY = "E_NO_ACTIVITY";
-    private static final String E_RUNTIME_ERROR = "E_RUNTIME_ERROR";
-    private static final String E_AUTH_ERROR = "E_AUTH_ERROR";
-    private static final String E_CANCELLED = "E_CANCELLED";
-    private static final String KEY_TOKEN = "token";
-    private static final String KEY_CODE = "code";
-    private static final String KEY_ERROR = "error";
-    private static final String KEY_STATE = "state";
-    private static final String KEY_EXPIRES_IN = "expiresIn";
-    private static final String EVENT_AUDIO_FLASH = "kSpPlaybackEventAudioFlush";
-
-    private static final String EVENT_DELIVERY_DOUN = "kSpPlaybackNotifyAudioDeliveryDone";
-
-    private static final String EVENT_BECAME_ACTIVE ="kSpPlaybackNotifyBecameActive";
-    private static final String EVENT_BECAME_INACTIVE = "kSpPlaybackNotifyBecameInactive";
-    private static final String EVENT_CONTEXT_CHANGED ="kSpPlaybackNotifyContextChanged";
-    private static final String EVENT_LOST_PERMISSION = "kSpPlaybackNotifyLostPermission";
+    private static final String E_NO_ACTIVITY          = "E_NO_ACTIVITY";
+    private static final String E_RUNTIME_ERROR        = "E_RUNTIME_ERROR";
+    private static final String E_AUTH_ERROR           = "E_AUTH_ERROR";
+    private static final String E_CANCELLED            = "E_CANCELLED";
+    private static final String KEY_TOKEN              = "token";
+    private static final String KEY_CODE               = "code";
+    private static final String KEY_ERROR              = "error";
+    private static final String KEY_STATE              = "state";
+    private static final String KEY_EXPIRES_IN         = "expiresIn";
+    private static final String EVENT_AUDIO_FLASH      = "kSpPlaybackEventAudioFlush";
+    private static final String EVENT_DELIVERY_DOUN    = "kSpPlaybackNotifyAudioDeliveryDone";
+    private static final String EVENT_BECAME_ACTIVE    = "kSpPlaybackNotifyBecameActive";
+    private static final String EVENT_BECAME_INACTIVE  = "kSpPlaybackNotifyBecameInactive";
+    private static final String EVENT_CONTEXT_CHANGED  = "kSpPlaybackNotifyContextChanged";
+    private static final String EVENT_LOST_PERMISSION  = "kSpPlaybackNotifyLostPermission";
     private static final String EVENT_METADATA_CHANGED = "kSpPlaybackNotifyMetadataChanged";
-    private static final String EVENT_NOTIFY_NEXT ="kSpPlaybackNotifyNext";
-    private static final String EVENT_NOTIFY_PAUSE = "kSpPlaybackNotifyPause";
-    private static final String EVENT_NOTIFY_PLAY ="kSpPlaybackNotifyPlay";
-    private static final String EVENT_NOTIFY_PREV = "kSpPlaybackNotifyPrev";
-    private static final String EVENT_REPEAT_OFF="kSpPlaybackNotifyRepeatOff";
-    private static final String EVENT_REPEAT_ON="kSpPlaybackNotifyRepeatOn";
-    private static final String EVENT_SHUFLE_OFF="kSpPlaybackNotifyShuffleOff";
-    private static final String EVENT_SHUFLE_ON = "kSpPlaybackNotifyShuffleOn";
-    private static final String EVENT_TRACK_CHANGED = "kSpPlaybackNotifyTrackChanged";
-    private static final String EVENT_TRACK_DELIVERED = "kSpPlaybackNotifyTrackDelivered";
+    private static final String EVENT_NOTIFY_NEXT      = "kSpPlaybackNotifyNext";
+    private static final String EVENT_NOTIFY_PAUSE     = "kSpPlaybackNotifyPause";
+    private static final String EVENT_NOTIFY_PLAY      = "kSpPlaybackNotifyPlay";
+    private static final String EVENT_NOTIFY_PREV      = "kSpPlaybackNotifyPrev";
+    private static final String EVENT_REPEAT_OFF       = "kSpPlaybackNotifyRepeatOff";
+    private static final String EVENT_REPEAT_ON        = "kSpPlaybackNotifyRepeatOn";
+    private static final String EVENT_SHUFLE_OFF       = "kSpPlaybackNotifyShuffleOff";
+    private static final String EVENT_SHUFLE_ON        = "kSpPlaybackNotifyShuffleOn";
+    private static final String EVENT_TRACK_CHANGED    = "kSpPlaybackNotifyTrackChanged";
+    private static final String EVENT_TRACK_DELIVERED  = "kSpPlaybackNotifyTrackDelivered";
 
-    /*
-     * setup
-     */
-    private String clientId = "";
-    private String redirectUri = "";
-    private String type;
-    private String access_token;
-    private String currentTrack;
-    private Long position;
-    private String nextTrack = "spotify:track:6rPO02ozF3bM7NnOV4h6s2";
+    private String type;         // type of request, token or code
+    private String clientId;     // clientId from spotify app
+    private String nextTrack;    // next track to play
+    private String redirectUri;  // redirect uri
+    private String access_token; //access token to can play music
 
-    private Metadata mMetaData;
-    private PlaybackState mCurrentPlaybackState;
-
-    /*
-     * request code
-     */
     private static final int REQUEST_CODE = 1337;
     public ReactApplicationContext reactContext;
-
-    /*
-     * player
-     */
     private SpotifyPlayer mPlayer;
 
     private final List<Promise> promises = Collections.synchronizedList(new ArrayList<Promise>());
     private final Player.OperationCallback mOperationCallback = new Player.OperationCallback() {
+
         @Override
         public void onSuccess() {
             Log.e("onSuccess", "OperationCallback");
@@ -115,18 +101,14 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
 
     public RNSdkSpotifyModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        //reactContext = _reactContext;
         reactContext.addActivityEventListener(this);
     }
 
     @ReactMethod
-    public void setAccessToken(final String access_token, final Promise promise) {
-        if(access_token != null)
-        {
-            onAuth(access_token, promise);
-        }
-        else
-        {
+    public void setAccessToken(final String _access_token, final Promise promise) {
+        if(_access_token != null) {
+            onAuth(_access_token, promise);
+        } else {
             promise.reject("Access token is undefined");
         }
     }
@@ -136,22 +118,22 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
         mPlayer.queue(new Player.OperationCallback() {
             @Override
             public void onSuccess() {
-                // Toast.makeText(SoundspaceActivity.this, "Added to queue!", Toast.LENGTH_SHORT).show();
+                Log.e("Success", "queue");
             }
 
             @Override
             public void onError(Error error) {
-                // fail
+                Log.e("Fail", "queue");
             }
         }, uri);
     }
 
-    private void onAuth(final String access_token, final Promise promise) {
+    private void onAuth(final String _access_token, final Promise promise) {
         Activity currentActivity = getCurrentActivity();
         // Once we have obtained an authorization token, we can proceed with creating a Player.
         // logStatus("Got authentication token");
         if (mPlayer == null) {
-            Config playerConfig = new Config(currentActivity, access_token, clientId);
+            Config playerConfig = new Config(currentActivity, _access_token, clientId);
             // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
             // the second argument in order to refcount it properly. Note that the method
             // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
@@ -161,12 +143,9 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
                 @Override
                 public void onInitialized(SpotifyPlayer player) {
                     mPlayer = player;
-                    // logStatus("-- Player initialized --");
-                    // mPlayer.setConnectivityStatus(mOperationCallback, RNSdkSpotifyModule.this);
                     mPlayer.addNotificationCallback(RNSdkSpotifyModule.this);
                     mPlayer.addConnectionStateCallback(RNSdkSpotifyModule.this);
-                    mPlayer.login(access_token);
-                    // resolve(authResponse);
+                    mPlayer.login(_access_token);
                     promise.resolve("true");
                 }
 
@@ -177,7 +156,7 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
                 }
             });
         } else {
-            mPlayer.login(access_token);
+            mPlayer.login(_access_token);
             promise.resolve("true");
         }
     }
@@ -266,21 +245,16 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
 
     @ReactMethod
     public AuthenticationRequest getAuthenticationRequest() {
-        Type __type;
+        Type __type = AuthenticationResponse.Type.TOKEN;
 
-        if(type == "code")
-        {
+        if(type == "code") {
             __type = AuthenticationResponse.Type.CODE;
-        }
-        else
-        {
-            __type = AuthenticationResponse.Type.TOKEN;
         }
 
         return new AuthenticationRequest.Builder(clientId, __type, redirectUri)
-                .setShowDialog(false)
-                .setScopes(new String[]{"user-read-email"})
-                .build();
+            .setShowDialog(false)
+            .setScopes(new String[]{"user-read-email", "playlist-read-private", "playlist-read-collaborative", "playlist-modify-public", "playlist-modify-private", "streaming"})
+            .build();
     }
 
     @ReactMethod
@@ -302,19 +276,15 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
     {
         Activity currentActivity = getCurrentActivity();
 
-        Type __type;
-        if(type == "code")
-        {
+        Type __type = AuthenticationResponse.Type.TOKEN;
+
+        if(type == "code") {
             __type = AuthenticationResponse.Type.CODE;
-        }
-        else
-        {
-            __type = AuthenticationResponse.Type.TOKEN;
         }
 
         final AuthenticationRequest request = new AuthenticationRequest.Builder(clientId, __type, redirectUri)
-                .setScopes(new String[]{"user-read-private", "playlist-read", "playlist-read-private", "streaming"})
-                .build();
+            .setScopes(new String[]{"user-read-email", "playlist-read-private", "playlist-read-collaborative", "playlist-modify-public", "playlist-modify-private", "streaming"})
+            .build();
 
         AuthenticationClient.openLoginActivity(currentActivity, REQUEST_CODE, request);
     }
@@ -323,7 +293,6 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
         Log.i("TAG", status);
     }
 
-    // @Override
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
@@ -331,7 +300,6 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
                 // Response was successful and contains auth token
                 case TOKEN:
                     onAuthenticationComplete(response);
-                    // resolve(response);
                     break;
                 case CODE:
                     resolve(response);
@@ -349,6 +317,7 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
         ConnectivityManager connectivityManager;
         connectivityManager = (ConnectivityManager) reactContext.getSystemService(ReactContext.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
         if (activeNetwork != null && activeNetwork.isConnected()) {
             return Connectivity.fromNetworkType(activeNetwork.getType());
         } else {
@@ -359,21 +328,13 @@ public class RNSdkSpotifyModule extends ReactContextBaseJavaModule
     private void onAuthenticationComplete(final AuthenticationResponse authResponse) {
         access_token = authResponse.getAccessToken();
         Activity currentActivity = getCurrentActivity();
-        // Once we have obtained an authorization token, we can proceed with creating a Player.
-        // logStatus("Got authentication token");
+
         if (mPlayer == null) {
             Config playerConfig = new Config(currentActivity, authResponse.getAccessToken(), clientId);
-            // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
-            // the second argument in order to refcount it properly. Note that the method
-            // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
-            // one passed in here. If you pass different instances to Spotify.getPlayer() and
-            // Spotify.destroyPlayer(), that will definitely result in resource leaks.
             mPlayer = Spotify.getPlayer(playerConfig, currentActivity, new SpotifyPlayer.InitializationObserver() {
                 @Override
                 public void onInitialized(SpotifyPlayer player) {
                     mPlayer = player;
-                    // logStatus("-- Player initialized --");
-                    // mPlayer.setConnectivityStatus(mOperationCallback, RNSdkSpotifyModule.this);
                     mPlayer.addNotificationCallback(RNSdkSpotifyModule.this);
                     mPlayer.addConnectionStateCallback(RNSdkSpotifyModule.this);
                     mPlayer.login(authResponse.getAccessToken());
--- android/src/main/java/com/reactlibrary/RNSdkSpotifyPackage.java
+++ android/src/main/java/com/reactlibrary/RNSdkSpotifyPackage.java
@@ -1,34 +1,36 @@
-
 package com.reactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-
-import java.util.ArrayList;
+import com.facebook.react.bridge.ReactApplicationContext;
 
 public class RNSdkSpotifyPackage implements ReactPackage {
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
+
         modules.add(new RNSdkSpotifyModule(reactContext));
+
         return modules;
-      // return Arrays.<NativeModule>asList(new RNSdkSpotifyModule(reactContext));
     }
 
     @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+
+        return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+
+        return Collections.emptyList();
     }
 }
